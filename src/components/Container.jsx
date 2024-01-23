5461a74334a0dfb15cafbaac8e10a63ebed99ea1
fernando <ffba97@gmail.com>
2024-01-23 12:11:20 -0300

    Fix container para mobile

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
 export const Container = ({ children }) =>
-    <div className="max-w-screen-xl mx-auto mt-4 p-4 border-solid border rounded-md shadow-lg">
+    <div className="max-w-screen-xl mx-auto mt-4 p-0 pb-4 sm:p-4 border-solid sm:border sm:rounded-md sm:shadow-lg">
         {children}
     </div>
