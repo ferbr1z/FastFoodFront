ec0e83f550d04e503d9fb6ca79bb82865b823ad6
fernando <ffba97@gmail.com>
2024-01-23 03:09:58 -0300

    Paginacion para resultados de busqueda

--- src/components/utils/Pagination.jsx
+++ src/components/utils/Pagination.jsx
@@ -1,21 +1,22 @@
 import { Pagination } from "flowbite-react";
 import { usePageDate } from "../../hooks/usePageData";
 import { useNavigate, useLocation } from "react-router-dom";
-export const PaginationFooter = () => {
-    const { page, totalPages } = usePageDate();
+import { useEffect } from "react";
+export const PaginationFooter = ({ className, search, handlePageChange }) => {
+    const { page, totalPages, totalSearchPages } = usePageDate();
 
     const location = useLocation();
     const navigation = useNavigate();
 
 
-    const handlePageChange = (page) => {
+    const handlePageChangeDefault = (page) => {
         navigation(`${page}`);
     }
 
     return (
-        <div className="flex flex-col items-center pt-3">
-            <Pagination currentPage={page} totalPages={totalPages} onPageChange={handlePageChange} showIcons nextLabel="" previousLabel="" />
-            <p className="mt-2">Página <b>{page}</b> de <b>{totalPages}</b></p>
+        <div className={`flex flex-col items-center mt-3 ${className} ${search && totalSearchPages === 1 ? "hidden" : ""}`}>
+            <Pagination currentPage={page} totalPages={!search ? totalPages : totalSearchPages} onPageChange={handlePageChange ?? handlePageChangeDefault} showIcons nextLabel="" previousLabel="" />
+            {search ? "" : <p className="mt-2">Página <b>{page}</b> de <b>{totalPages}</b></p>}
         </div>
     );
 }; 
