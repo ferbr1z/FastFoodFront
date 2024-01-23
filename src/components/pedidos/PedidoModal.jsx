3234484e81fb1f7519c8d2b977ff642a70d23628
fernando <ffba97@gmail.com>
2024-01-23 17:19:18 -0300

    PedidoModal + Search terminado

--- src/components/pedidos/PedidoModal.jsx
+++ src/components/pedidos/PedidoModal.jsx
@@ -6,11 +6,12 @@ import { EstadosPedidos } from "../utils/EstadosPedidos";
 import { usePedidos } from "../../hooks/usePedidos";
 import { ProductSearch } from "./ProductSearch";
 import { H3 } from "../utils/headers/H3";
+import { HiXCircle } from "react-icons/hi";
 export const PedidoModal = ({ data, modal, setData, closeModal }) => {
 
     const [showAlert, setShowAlert] = useState(false);
     const [nombreInputColor, setNombreInputColor] = useState("")
-    const [newPedidoData, setNewPedidoData] = useState({ id: 0, nombre: "", direccion: "", detalles: [] });
+    const [alertMsg, setAlertMsg] = useState("");
     const { addPedido, updatePedido, failedState } = usePedidos();
     const { showErrorModal } = useErrorModal();
 
@@ -24,7 +25,7 @@ export const PedidoModal = ({ data, modal, setData, closeModal }) => {
 
     useEffect(() => {
         if (showAlert === false) return;
-        if (data.nombre === '') {
+        if (data.nombreCliente === '') {
             setNombreInputColor("failure");
         } else {
             setNombreInputColor("");
@@ -47,33 +48,35 @@ export const PedidoModal = ({ data, modal, setData, closeModal }) => {
 
     const handleSave = () => {
 
+        {console.log(data.pedidoDetalle.length)}
+
         if (data.nombreCliente === '') {
             setShowAlert(true);
+            setAlertMsg("El nombre del cliente no puede estar vacío.");
             setNombreInputColor("failure");
             return;
         }
 
+        if (data.pedidoDetalle.length === 0) {
+            setShowAlert(true);
+            setAlertMsg("El pedido no puede estar vacío.");
+            return;
+        }
+       
         if (data.nombreCliente === '') {
             setShowAlert(true);
             return;
         }
 
-        if (data.direccion === '') {
-            setShowAlert(true);
-            return;
-        }
+
 
         if (modal.update) {
-            if (data.nombreCliente === newPedidoData.nombreCliente && data.direccion === newPedidoData.direccion) {
-                closeAndResetValues();
-                return;
-            }
-
             // If the user only changed one of the values, we only send that value to the backend
             const newPedidoObject = {};
             newPedidoObject.id = data.id;
             newPedidoObject.nombreCliente = data.nombreCliente;
             newPedidoObject.direccion = data.direccion;
+            newPedidoObject.pedidoDetalle = data.pedidoDetalle;
 
             updatePedido(newPedidoObject);
         }
@@ -83,17 +86,88 @@ export const PedidoModal = ({ data, modal, setData, closeModal }) => {
         closeAndResetValues();
     }
 
+    const handleProductoCantidadChange = (e, producto) => {
+        setData({
+            ...data,
+            pedidoDetalle: data.pedidoDetalle.map((detalle) => {
+                if (detalle.producto.id === producto.id) {
+                    return { ...detalle, cantidad: e.target.value };
+                }
+                return detalle;
+            })
+        })
+    }
+
+    const handleRemoveProducto = (id) => {
+        setData({
+            ...data,
+            pedidoDetalle: data.pedidoDetalle.filter((detalle) => detalle.producto.id !== id)
+        })
+    }
+
     const closeAndResetValues = () => {
         setShowAlert(false);
         setNombreInputColor("");
         closeModal();
     }
 
+    const pedidoTableEmpty = () => {
+        return <div className="flex justify-center items-center p-4 sm:mt-2 rounded-md bg-white text-center">
+            <H3 color="gray">Aún hay productos en el pedido</H3>
+        </div>
+    }
+
+    const pedidoTable = () => {
+        return <>
+            <Table className="sm:mt-2 rounded-md bg-white overflow-x-auto">
+                <TableHead className="hidden sm:table-header-group">
+                    <TableHeadCell>Producto</TableHeadCell>
+                    <TableHeadCell>Cantidad</TableHeadCell>
+                    <TableHeadCell>Total</TableHeadCell>
+                    <TableHeadCell></TableHeadCell>
+                </TableHead>
+                <TableBody>
+                    {
+                        data.pedidoDetalle.map((detalle) => {
+                            return <TableRow className="border-b">
+                                <TableCell>{detalle.producto.nombre}</TableCell>
+                                <TableCell> <TextInput type="number" value={detalle.cantidad} className="w-20 max-w-full text-center" onChange={(e) => handleProductoCantidadChange(e, detalle.producto)} /> </TableCell>
+                                <TableCell>Gs.<b>{(detalle.producto.precio * detalle.cantidad)}</b></TableCell>
+                                <TableCell>
+                                    <a className="hover:text-red-600 block p-3" title="Borrar de la lista" onClick={() => handleRemoveProducto(detalle.producto.id)} >
+                                        <HiXCircle className="w-5 h-5 mx-0" />
+                                    </a>
+                                </TableCell>
+                            </TableRow>
+                        })
+                    }
+                </TableBody>
+            </Table>
+            <div className=" bg-yellow-50 text-yellow-700 p-3 sm:mx-10 my-4 rounded-md shadow-xl">
+                <div className="grid grid-cols-2 sm:px-3 text-center">
+                    <span className="text-xl font-bold">Total</span>
+                    <p>Gs. <span className="text-xl font-bold">
+                        {
+                            data.pedidoDetalle.reduce((pedido, suma) => (pedido + (suma.producto.precio * suma.cantidad)), 0
+                            )}
+                    </span></p>
+                </div>
+            </div>
+        </>
+    }
+
+    const pedidoTableContent = () => {
+        if (data.pedidoDetalle.length === 0) {
+            return pedidoTableEmpty();
+        }
+        return pedidoTable();
+    }
+
     return (
         <>
             <ModalBase title={modal.new ? "Agregar un pedido" : "Editar pedido"} open={modal.new || modal.update} closeModal={closeAndResetValues} onKeyDown={handleKeyDown}>
                 <Alert color="red" className={`mb-6 ${showAlert === true ? "" : "hidden"}`}>
-                    <p>Los campos no pueden estar vacios</p>
+                    <p>{alertMsg}</p>
                 </Alert>
                 <div className="mb-8 space-y-6">
                     <div className="grid grid-cols-2 gap-3">
@@ -114,40 +188,9 @@ export const PedidoModal = ({ data, modal, setData, closeModal }) => {
                         </div>
                     </div>
 
-                    <div className="sm:p-3 rounded-md bg-slate-50 overflow-x-auto ">
-                        <ProductSearch />
-                        <Table className="sm:mt-2 rounded-md bg-white">
-                            <TableHead className="hidden sm:table-header-group">
-                                <TableHeadCell>Producto</TableHeadCell>
-                                <TableHeadCell>Cantidad</TableHeadCell>
-                                <TableHeadCell>Total</TableHeadCell>
-                            </TableHead>
-                            <TableBody>
-                                <TableRow className="border-b">
-                                    <TableCell>Hamburguesa Tradicional</TableCell>
-                                    <TableCell> <TextInput type="number" value={100} className="w-20 max-w-full text-center" onChange={() => { }} /> </TableCell>
-                                    <TableCell>30000</TableCell>
-                                </TableRow>
-
-                                <TableRow className="border-b">
-                                    <TableCell>Hamburguesa Tradicional</TableCell>
-                                    <TableCell> <TextInput type="number" value={100} className="w-20 max-w-full text-center" onChange={() => { }} /> </TableCell>
-                                    <TableCell>30000</TableCell>
-                                </TableRow>
-                                <TableRow className="border-b">
-                                    <TableCell>Hamburguesa Tradicional</TableCell>
-                                    <TableCell> <TextInput type="number" value={100} className="w-20 max-w-full text-center" onChange={() => { }} /> </TableCell>
-                                    <TableCell>30000</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </div>
-
-                    <div className=" bg-yellow-50 text-yellow-700 p-3 sm:mx-10 rounded-md shadow-xl">
-                        <div className="grid grid-cols-2 sm:px-3 text-center">
-                            <span className="text-xl font-bold">Total</span>
-                            <p>Gs. <span className="text-xl font-bold">60000</span></p>
-                        </div>
+                    <div className="sm:p-3 rounded-md bg-slate-50">
+                        <ProductSearch pedido={data} setPedido={setData} />
+                        {pedidoTableContent()}
                     </div>
 
                 </div>
--- src/components/pedidos/ProductSearch.jsx
+++ src/components/pedidos/ProductSearch.jsx
@@ -1,12 +1,11 @@
-import { Table, TableBody, TableRow, TextInput, TableCell, TableHead, TableHeadCell } from "flowbite-react";
+import { Table, TableBody, TableRow, TextInput, TableCell } from "flowbite-react";
 import { useEffect, useState } from "react"
 import { useProducts } from "../../hooks/useProducts";
 import { PaginationFooter } from "../utils/Pagination";
-import { H3 } from "../utils/headers/H3";
 import { Loading } from "../utils/Loading";
 import { HighlightSearchResult } from "../utils/HighlightSearchResult";
 
-export const ProductSearch = () => {
+export const ProductSearch = ({ pedido, setPedido }) => {
 
     const [productoNombre, setProductoNombre] = useState("");
     const [productoSearchPage, setProductoSearchPage] = useState(1);
@@ -16,38 +15,44 @@ export const ProductSearch = () => {
         setProductoNombre(e.target.value);
     }
 
+    const handleProductClick = (producto) => {
+        setPedido({ ...pedido, pedidoDetalle: [...pedido.pedidoDetalle, { producto: producto, cantidad: 1 }] });
+        setProductoNombre("");
+        setProductoSearchPage(1);
+    }
+
     useEffect(() => {
         if (productoNombre.length < 3) return;
         findProductByName(productoNombre, productoSearchPage);
     }, [productoNombre, productoSearchPage]);
 
     const searchResult = () => {
-    
-        if (productos.length === 0) return <p className="text-center m-3 font-bold text-gray-500">No se han encontrado coincidencias.</p>;
 
+        
         if (loadedState === false) return <Loading minimal />;
+        if (productos.length === 0) return <p className="text-center m-3 font-bold text-gray-500">No se han encontrado coincidencias.</p>;
 
         return (<>
             <Table className="drop-shadow-none">
                 <TableBody>
-                    {productos.map((producto) => {
-                        return <TableRow key={producto.id} className="hover:bg-gray-50">
+                    {productos.map((producto, i) => {
+                        return <TableRow key={producto.id} onClick={() => handleProductClick(producto)} className={`hover:bg-gray-50 active:bg-gray-200 ${i < (productos.length - 1) ? "border-b" : ""}`}>
                             <TableCell>
                                 <HighlightSearchResult text={producto.nombre} highlightText={productoNombre} />
                             </TableCell>
-                            <TableCell className="font-bold text-red-600">{producto.precio}</TableCell>
+                            <TableCell>Gs. <span className="font-bold text-red-600">{producto.precio}</span> </TableCell>
                         </TableRow>
                     })
                     }
                 </TableBody>
             </Table>
-            <PaginationFooter className="mt-0" search handlePageChange={(page)=>setProductoSearchPage(page)} /></>)
+            <PaginationFooter className="mt-0" search handlePageChange={(page) => setProductoSearchPage(page)} /></>)
     }
 
     return (
         <>
             <div className="relative">
-                <TextInput type="search" id="productoSearch" value={productoNombre} onChange={handleSearch} />
+                <TextInput color="light" type="search" id="productoSearch" value={productoNombre} onChange={handleSearch} placeholder="Escribe el nombre del producto que deseas buscar..." />
 
                 {/* Resultado de búsqueda */}
                 <div className={`absolute z-40 bg-white w-full mt-1 p-2 rounded-md  shadow-lg ${productoNombre.length < 3 ? "hidden" : ""} `}>
