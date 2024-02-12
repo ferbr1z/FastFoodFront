705fde9d066f8df8deddd18c1f2b73408effa455
Fernando Brizuela <ffba97@gmail.com>
2024-02-12 00:59:05 -0300

    (+) Fetching de pedidos segun estado + Fix de renderización del div anterior a la tabla

--- src/components/pedidos/PedidosList.jsx
+++ src/components/pedidos/PedidosList.jsx
@@ -1,11 +1,11 @@
-import { Button, ButtonGroup, Label, Radio, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react"
+import { Button, ButtonGroup, Label, Radio, Select, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react"
 import { useEffect, useState } from "react"
 import { Loading } from "../utils/Loading"
 import { H2 } from "../utils/headers/H2"
 import { PedidoModal } from "./PedidoModal"
 import { useParams } from "react-router-dom"
 import { PaginationFooter } from "../utils/Pagination"
-import { HiPlus, HiEye, HiXCircle, HiFilter } from "react-icons/hi";
+import { HiPlus, HiEye, HiXCircle, HiFilter, HiOutlineCheckCircle } from "react-icons/hi";
 import { RemoveModal } from "../utils/Modals/EraseModal"
 import { usePedidos } from "../../hooks/usePedidos"
 
@@ -13,13 +13,10 @@ import { usePedidos } from "../../hooks/usePedidos"
 export const PedidosList = () => {
 
     const { page } = useParams();
-    const { pedidos, getAllPedidos, getPedidoById, getPedidosEntregados, deletePedido, loadedState, failedState, failedOnLoadState } = usePedidos();
+    const { pedidos, getAllPedidos, getPedidoById, getPedidosEntregados, getPedidosCancelados, deletePedido, loadedState, failedState, failedOnLoadState } = usePedidos();
     const [modal, setModal] = useState({ new: false, update: false, remove: false });
     const [pedidoData, setPedidoData] = useState({ id: 0, nombreCliente: "", estado: "", direccion: "", pedidoDetalle: [] });
-
-    const getAll = async () => {
-        await getAllPedidos(page);
-    }
+    const [fetchEstado, setFetchEstado] = useState("pendientes");
 
     const closeModal = () => { setModal({ ...modal, new: false, update: false, remove: false }) };
 
@@ -54,9 +51,33 @@ export const PedidosList = () => {
         deletePedido(id);
     }
 
+    const getPedidos = async (page) => {
+        switch (fetchEstado) {
+            case "":
+                await getAllPedidos(page);
+                break;
+            case "pendientes":
+                await getAllPedidos(page);
+                break;
+            case "entregados":
+                await getPedidosEntregados(page);
+                break;
+            case "cancelados":
+                await getPedidosCancelados(page);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const handleEstadoChange = (estado) => {
+        setFetchEstado(estado);
+        console.log(fetchEstado);
+    }
+
     useEffect(() => {
-        getAll();
-    }, [page])
+        getPedidos(page);
+    }, [page, fetchEstado])
 
     const showMessage = () => {
         if (failedOnLoadState === true) {
@@ -71,13 +92,24 @@ export const PedidosList = () => {
                         <PaginationFooter />
                     </center>
 
-                return <div className="h-100 w-100 flex items-center justify-center my-80"><center>
-                    <H2 color="gray">No hay pedidos todavía</H2>
-                    <Button className="m-3" onClick={() => handleNew()}>
-                        <HiPlus />
-                        Nuevo pedido
-                    </Button>
-                </center></div>
+                if (fetchEstado === "") {
+                    return <div className="h-100 w-100 flex items-center justify-center my-80"><center>
+                        <H2 color="gray">No hay pedidos todavía</H2>
+                        <Button className="m-3" onClick={() => handleNew()}>
+                            <HiPlus />
+                            Nuevo pedido
+                        </Button>
+                    </center></div>
+                } else if (fetchEstado === "entregados" || fetchEstado === "cancelados") {
+                    return <>
+                        <div className="h-100 w-100 flex items-center justify-center my-80">
+                            <center>
+                                <H2 color="gray">Nada por aqui</H2>
+                            </center>
+                        </div>
+                    </>
+                }
+
             }
             return renderContent();
         }
@@ -93,28 +125,6 @@ export const PedidosList = () => {
     }
 
     const renderTable = () => (<>
-        <div className="flex justify-center md:justify-between mb-3">
-            <fieldset className="flex flex-row gap-4">
-                <div className="text-gray-700 inline-flex items-center">
-                    <HiFilter className="w-4 h-4 mx-0" /> Filtros:</div>
-                <div className="flex items-center gap-2">
-                    <Radio id="pendientes_filter" name="estado" onChange={() => getAllPedidosEntregados(page)} defaultChecked />
-                    <Label htmlFor="pendientes_filter">Pendientes</Label>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Radio id="entregados_filter" name="estado" onChange={() => getAllPedidosEntregados(page)} />
-                    <Label htmlFor="entregados_filter">Entregados</Label>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Radio id="cancelados_filter" name="estado" onChange={() => getAllPedidosEntregados(page)} />
-                    <Label htmlFor="cancelados_filter">Cancelados</Label>
-                </div>
-            </fieldset>
-            <Button className="" onClick={handleNew}>
-                <HiPlus className="mx-1 w-3 h-3" />
-                Nuevo pedido
-            </Button>
-        </div>
         <div className="drop-shadow-md rounded-md overflow-auto">
             <Table className="bg-white" hoverable>
                 <TableHead className="w-full">
@@ -128,15 +138,18 @@ export const PedidosList = () => {
                             <TableCell>{pedido.nombreCliente}</TableCell>
                             <TableCell className="">{pedido.direccion}</TableCell>
                             <TableCell>
-                                <div className="inline-flex sm:gap-2">
-                                    <Button color="light" outline onClick={() => handleEdit(pedido)}>
+                                <ButtonGroup>
+                                    <Button color="light" onClick={() => handleEdit(pedido)}>
                                         <HiEye className="w-4 h-4 mx-0 md:mr-1" />
                                         <span className="hidden md:block">Ver</span></Button>
-                                    <Button color="failure" outline onClick={() => handleErase(pedido)}>
+                                    <Button color="green">
+                                        <HiOutlineCheckCircle className="w-4 h-4 mx-0 md:mr-1" /><span className="hidden md:block"> Entregado</span>
+                                    </Button>
+                                    <Button color="red" onClick={() => handleErase(pedido)}>
                                         <HiXCircle className="w-4 h-4 mx-0 md:mr-1" />
                                         <span className="hidden md:block"> Cancelar</span>
                                     </Button>
-                                </div>
+                                </ButtonGroup>
                             </TableCell>
                         </TableRow>
                         )
@@ -146,6 +159,31 @@ export const PedidosList = () => {
         <PaginationFooter /></>)
 
     return <>
+        <><div className="flex flex-col items-center md:flex-row justify-center md:justify-between mb-3">
+
+            <Button className={`mb-3 md:order-2 md:mb-0 ${fetchEstado === "pendientes" ? "" : "hidden"}`} onClick={handleNew}>
+                <HiPlus className="mx-1 w-3 h-3" />
+                Nuevo pedido
+            </Button>
+
+            <fieldset className="flex flex-row gap-4">
+                <div className="text-gray-700 inline-flex items-center">
+                    <HiFilter className="w-4 h-4 mx-0" /><span className="hidden md:block">Filtros:</span></div>
+                <div className="flex items-center gap-2">
+                    <Radio id="pendientes_filter" name="estado" onChange={() => handleEstadoChange("pendientes")} defaultChecked />
+                    <Label htmlFor="pendientes_filter">Pendientes</Label>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Radio id="entregados_filter" name="estado" onChange={() => handleEstadoChange("entregados")} />
+                    <Label htmlFor="entregados_filter">Entregados</Label>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Radio id="cancelados_filter" name="estado" onChange={() => handleEstadoChange("cancelados")} />
+                    <Label htmlFor="cancelados_filter">Cancelados</Label>
+                </div>
+            </fieldset>
+
+        </div></>
         {showMessage()}
         <PedidoModal modal={modal} data={pedidoData} setData={setPedidoData} closeModal={closeModal} />
         <RemoveModal data={pedidoData} borrarTitulo={`el pedido de ${pedidoData.nombreCliente}?`} closeModal={closeModal} removeFunction={removeFuntion} open={modal.remove} failedState={failedState} />
